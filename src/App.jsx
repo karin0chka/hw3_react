552f76236d59ff66ddcc47eb5128ead60b261113
Karina <karina.kvych@gmail.com>
2023-07-27 19:59:39 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import WeatherSearch from './WeatherSearch'
+
+function App() {
+  return(
+    <>
+    <h1>Weather App</h1>
+    <WeatherSearch />
+    </>
+
+  )
+
+  
+}
+
+export default App
